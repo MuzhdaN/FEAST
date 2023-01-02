dc094b67ab893bafbeaf80816fe53b86d9c4ee95
MuzhdaN <muzhda.noorzad122@gmail.com>
2023-01-02 19:46:15 +0000

    Bug: changed code if app deploys

--- feast/settings.py
+++ feast/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['restaurant-booking-system.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ["restaurant-booking-system.herokuapp.com", "localhost"]
 
 
 # Application definition

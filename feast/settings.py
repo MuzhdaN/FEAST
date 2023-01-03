c3454723d32af7f002dd5238e70a25993c940629
MuzhdaN <muzhda.noorzad122@gmail.com>
2023-01-03 07:18:24 +0000

    Changed database locations and added reservation app in installed_apps

--- feast/settings.py
+++ feast/settings.py
@@ -10,10 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+
 import dj_database_url
-if os.path.isfile('env.py'):
+if os.path.isfile("env.py"):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'reservation',
 ]
 
 MIDDLEWARE = [

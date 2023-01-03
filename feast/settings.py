3219fd1a64b1eef95d48ac03cd216ef6d8e1f71b
MuzhdaN <muzhda.noorzad122@gmail.com>
2023-01-03 08:08:46 +0000

    Bug: changed debug line to false

--- feast/settings.py
+++ feast/settings.py
@@ -28,7 +28,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+### changed this from true to false as H10 app bug occurs
+DEBUG = False 
 
 ALLOWED_HOSTS = ["restaurant-booking-system.herokuapp.com", "localhost"]
 

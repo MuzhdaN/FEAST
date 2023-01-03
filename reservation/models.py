2f1498292522b270e518d935c3ecffbab6119a2e
MuzhdaN <muzhda.noorzad122@gmail.com>
2023-01-03 18:49:25 +0000

    Added admin and booking database features

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Table(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.IntegerField()
+    guest = models.IntegerField()
+    date = models.DateField()
+    time = models.TimeField()
+
+    def __str__(self):
+        return self.name
+
--- reservation/views.py
+++ reservation/views.py
@@ -1,3 +1,10 @@
+from .models import Table
 from django.shortcuts import render
+# from django.http import HttpResponse
+
+from reservation.models import Table
+
 
 # Create your views here.
+def reserve_table(request):
+    return render(request, 'reservation/reservation.html')

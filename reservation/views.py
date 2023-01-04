5b14c8e7cdb9b5c507d88bf16ae639ed8a4a519e
MuzhdaN <muzhda.noorzad122@gmail.com>
2023-01-04 13:05:28 +0000

    Added reserve_table function to submit the form

--- reservation/views.py
+++ reservation/views.py
@@ -1,10 +1,28 @@
 from .models import Table
-from django.shortcuts import render
-# from django.http import HttpResponse
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import messages
+from .forms import reserveTableForm
 
 from reservation.models import Table
 
 
 # Create your views here.
 def reserve_table(request):
-    return render(request, 'reservation/reservation.html')
+    booking_form = reserveTableForm()
+    
+    if request.method == 'POST':
+        booking_form = reserveTableForm(request.POST)
+
+        if booking_form.is_valid():
+            booking_form.save()
+            booking_form = reserveTableForm()
+            messages.success(
+                request, "Booking succesful")
+            return render(
+                # redirect to confirmation page
+                request, 'reservation/reservation.html', {'form': booking_form})       
+        else:
+            booking_form = reserveTableForm()
+            return render(request, 'reservation/reservation.html', {'form': booking_form})
+    context = {'form': booking_form}
+    return render(request, 'reservation/reservation.html', context)

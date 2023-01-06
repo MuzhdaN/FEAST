85091424dfe95101318cf370e61fd48061ca6e6c
MuzhdaN <muzhda.noorzad122@gmail.com>
2023-01-06 19:17:18 +0000

    Fixed typo error

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from home.views import Home
+from home.views import home
 
 app_name = 'home'
 
 
 urlpatterns = [
-    path('', Home.as_view(), name='home'),
+    path('', home.as_view(), name='home'),
 ]

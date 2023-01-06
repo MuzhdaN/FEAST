601ac681d34fff0ece7dd4e0ae66981bec641e38
MuzhdaN <muzhda.noorzad122@gmail.com>
2023-01-06 16:18:43 +0000

    Added crispy form

--- feast/settings.py
+++ feast/settings.py
@@ -48,7 +48,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'reservation',
+    'home',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

a6b6c504197ba355b3656ee95adaa4a01b862afe
MuzhdaN <muzhda.noorzad122@gmail.com>
2023-01-09 09:36:47 +0000

    Added message tags

--- feast/settings.py
+++ feast/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 import dj_database_url
 if os.path.isfile("env.py"):
@@ -61,6 +62,15 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# Build in tags with bootstrap classes
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+}
 
 # To prevent 500 errors during login and registration
 ACCOUNT_EMAIL_VERIFICATION = 'none'

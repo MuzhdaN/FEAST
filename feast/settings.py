af235271e82910e7bcbaa85cf8008a007ba9841f
MuzhdaN <muzhda.noorzad122@gmail.com>
2023-01-11 20:32:38 +0000

    Fixed error: Changed debug value as css wasn't loading

--- feast/settings.py
+++ feast/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = 'DEVELOPMENT'
 
 ALLOWED_HOSTS = ["restaurant-booking-system.herokuapp.com", "localhost"]
 

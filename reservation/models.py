6c15b8b335bed24085fb59bf92aac59e290c1ad9
MuzhdaN <muzhda.noorzad122@gmail.com>
2023-01-11 08:35:30 +0000

    Added a FK field

--- reservation/models.py
+++ reservation/models.py
@@ -13,7 +13,7 @@ class Table(models.Model):
     guest = models.IntegerField()
     date = models.DateField()
     time = models.TimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user", null=True)
 
     def __str__(self):
         return self.name
-
